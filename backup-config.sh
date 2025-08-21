4d17199bf407fe1f1de63943fea7a5d35b9f8b31
kalinka <kalinka@kalinka.ch>
2025-08-21 15:24:20 +0200

    update

--- backup-config.sh
+++ backup-config.sh
@@ -11,3 +11,7 @@ rsync -av $HOME/.gitconfig-work ./config-files
 mkdir -p config-files/.ssh/configs
 rsync -av $HOME/.ssh/configs/ ./config-files/.ssh/configs/
 rsync -av $HOME/.ssh/config ./config-files/.ssh
+
+mkdir -p etc/{keyd,ssh}
+rsync -av /etc/keyd/default.conf etc/keyd/default.conf
+sudo rsync -av --chown=kalinka:kalinka /etc/ssh/sshd_config etc/ssh/sshd_config
